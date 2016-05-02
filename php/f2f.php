3aeb6899c33b896ff7586f35b6c83813963749cb
duarte morais <duarte@debian>
2016-05-02 18:04:41 +0300

    begin little by little

---
+++ php/f2f.php
@@ -0,0 +1,11 @@
+<?php
+
+
+	/* include templates *//*
+	include_once("templates/header.php");  
+	include_once("templates/pollsList.php");
+	include_once("templates/list_polls.php");
+	include_once("templates/footer.php");
+*/
+?>
+
